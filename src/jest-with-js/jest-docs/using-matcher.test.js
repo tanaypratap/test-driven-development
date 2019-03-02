bba2c01055df256deac8cceecac9e666731bdae2
Tanay Pratap <tanaypratap@Phoenix-Soar.redmond.corp.microsoft.com>
2019-03-02 19:16:32 +0530

    done: all matchers test

--- src/jest-with-js/jest-docs/using-matcher.test.js
+++ src/jest-with-js/jest-docs/using-matcher.test.js
@@ -35,15 +35,58 @@ test('null', () => {
 	expect(n).toBeDefined();
 	expect(n).not.toBeUndefined();
 	expect(n).not.toBeTruthy();
-    expect(n).toBeFalsy();
+	expect(n).toBeFalsy();
 });
 
 test('zero', () => {
-    const z = 0;
-    expect(z).not.toBeNull();
-    expect(z).toBeDefined();
-    expect(z).not.toBeUndefined();
-    expect(z).not.toBeTruthy();
-    expect(z).toBeFalsy();
+	const z = 0;
+	expect(z).not.toBeNull();
+	expect(z).toBeDefined();
+	expect(z).not.toBeUndefined();
+	expect(z).not.toBeTruthy();
+	expect(z).toBeFalsy();
 });
 
+/**
+ * Numbers
+ */
+test('two plus two', () => {
+	const value = 2 + 2;
+	expect(value).toBeGreaterThan(3);
+	expect(value).toBeGreaterThanOrEqual(3.5);
+	expect(value).toBeLessThan(5);
+	expect(value).toBeLessThanOrEqual(4.5);
+
+	expect(value).toBe(4);
+	expect(value).toEqual(4);
+});
+
+/**
+ * Floats
+ */
+test('adding floating point numbers', () => {
+	const decimalNum = 0.1 + 0.2;
+
+	expect(decimalNum).toBeCloseTo(0.3);
+});
+
+/**
+ * Strings
+ */
+test('there is no I in team', () => {
+	expect('team').not.toMatch(/I/);
+});
+
+test('but there is a nay in Tanay', () => {
+	expect('Tanay').toMatch(/nay/);
+});
+
+/**
+ * Arrays
+ */
+
+const todoItems = ['shopping', 'laundry', 'side-project', 'book-reading'];
+
+test('the todo has shopping in it', () => {
+	expect(todoItems).toContain('shopping');
+});
