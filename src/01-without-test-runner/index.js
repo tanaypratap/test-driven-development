8377bf383b064d85bc44fbb5b963ecd6c2866dee
Tanay Pratap <tanaypratap@Phoenix-Soar.redmond.corp.microsoft.com>
2019-03-02 15:42:59 +0530

    folder name change

--- src/01-without-test-runner/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Import all tests to run
- */
-
-console.clear();
-
-const itemTests = require("./01_2_cart-handler/item.test");
-const cartFunctionTests = require("./01_1_cart-function/cart.test");
-const cartHandlerTests = require("./01_2_cart-handler/cart-handler.test");
---
+++ src/pure-js-tests/index.js
@@ -0,0 +1,9 @@
+/**
+ * Import all tests to run
+ */
+
+console.clear();
+
+const itemTests = require("./cart_as_class/item.test");
+const cartFunctionTests = require("./cart_as_function/cart.test");
+const cartHandlerTests = require("./cart_as_class/cart-handler.test");
